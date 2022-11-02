b725df037ec82787426f551f192ab060c1a544cd
Andrew McKnight <andrew.mcknight@sentry.io>
2022-11-01 20:01:34 -0800

    add other plugins

---
+++ _plugins/read-time.rb
@@ -0,0 +1,17 @@
+# from https://51bits.com/notes/estimated-reading-times-in-jekyll/
+module ReadTimeFilter
+
+    def readtime(input)
+
+        charcount = 4.5
+        wpm = 180
+
+        rt = (input.to_f/charcount/wpm).round
+        rt = 1 if rt < 1
+        rt
+
+    end
+
+    Liquid::Template.register_filter self
+
+end
